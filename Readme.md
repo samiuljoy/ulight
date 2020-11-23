602220b17587cbb655b8aff2fb4ff97d069c9afc
samiuljoy <samiuljoy46@gmail.com>
2020-11-23 06:40:31 -0700

    Ulight browser

---
+++ Readme.md
@@ -0,0 +1,1 @@
+This is Ulight browser. A lightweight browser that comes by default on Puppy Linux. The original project exists on https:// 
---
+++ browser/defaults/preferences/local-settings.js
@@ -0,0 +1,3 @@
+//
+//default home page
+pref("browser.startup.homepage", "data:text/plain,browser.startup.homepage=file:///usr/share/doc/home.htm");
